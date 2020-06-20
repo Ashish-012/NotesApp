c54612396339104f484cbab0afd5771a976b866d
unknown <contactashishjoshi99@gmail.com>
2020-06-20 19:38:06 +0530

    added index.php

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href='css/style.css' rel='stylesheet'>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Notes App</title>
+    <link href='https://fonts.googleapis.com/css?family=Arima Madurai' rel='stylesheet'>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-custom navbar-expand-md navbar-fixed-top navbar-light" style="background-color: #209fde;">
+    <a class="navbar-brand" href="index.php" style = 'color:white;margin-right:80px' >NOTES</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="nav navbar-nav navbar-right">
+        <li class="active">
+            <a class="nav-link" style='color:white;padding-right:25px;' href="#" >Home</a>
+        </li>
+        <li>
+            <a class="nav-link" style='color:white;padding-right:25px;' href="#" >Help</a>
+        </li>
+        <li>
+            <a class="nav-link" style='color:white;' href="#" >Contact Us</a>
+        </li>
+        </ul>
+        
+        <ul class="nav navbar-nav ml-auto">
+        <li>
+            <a class="nav-link" style='color:white;' href="#" >Login</a>
+        </li>
+        
+        </ul>
+        
+    </div>
+    </nav> 
+    <div class="jumbotron" id='myContainer'>
+            <h1>Online Notes App</h1>
+            <h3 style='margin-top:50px'>Your Notes with you wherever you go</h3>
+            <a class="btn btn-success btn-lg" href="#" style='margin-top:50px;' role="button">Sign up - Its Free!</a>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
